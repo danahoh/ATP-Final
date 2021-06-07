70fe1b8c2f9f03556e6e5fff52895ee0d0e35ebc
navitbranin <navitbranin123@gmail.com>
2021-06-07 16:40:40 +0300

    zoom

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -15,25 +15,25 @@ public class MainView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("NewFXML.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MazeStage.fxml"));
         //Pane root = FXMLLoader.load(getClass().getResource("NewFXML.fxml"));
         Parent root = fxmlLoader.load();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root,1000,700));
-        //Scene scene = new Scene(root,1000,700);
-        primaryStage.show();
+        //primaryStage.setScene(new Scene(root,1000,700));
+        Scene scene = new Scene(root,1000,700);
 
         IModel model = new MyModel();
         MyViewModel myViewModel = new MyViewModel(model);
         MyViewController view = fxmlLoader.getController();
         view.setViewModel(myViewModel);
 
-//        root.prefWidthProperty().bind(scene.widthProperty());
-//        root.prefHeightProperty().bind(scene.heightProperty());
+//       root.prefWidthProperty().bind(scene.widthProperty());
+//       root.prefHeightProperty().bind(scene.heightProperty());
 //
 //
 //
-//        primaryStage.setScene(scene);
+       primaryStage.setScene(scene);
+       primaryStage.show();
 
     }
 
